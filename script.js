6d0fa9ccb6acff273a93213e6d3a1e6642b88db7
srdjan6 <117856862+srdjan6@users.noreply.github.com>
2023-05-16 19:04:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ console.log("hello from git repository");
 // git status - provjerava da li ima promjena u projektu na racunaru
 // git add "ime file-a" ili -A je za sve fajlove - dodaje nove fajlove na projekat 
 // git push - pushamo fajl sa racunara na server
+// git pull - ce da povuce nove promjene koje jos nismo povukli sa servera
