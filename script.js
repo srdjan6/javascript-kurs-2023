ed69d21107e6f108cddb3fed1fbadf8cda08163a
srdjan6 <savanovicsrdjan39@gmail.com>
2023-05-16 18:48:56 +0200

    srdjan

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("hello from git repository");
+
+
+// git clone "linkrepositurijuma" - klonira repos na racunar
+// git status - provjerava da li ima promjena u projektu na racunaru
+// git add "ime file-a" ili -A je za sve fajlove - dodaje nove fajlove na projekat 
+// git push - pushamo fajl sa racunara na server
